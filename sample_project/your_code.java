42557ae8b6b381b73e4531102074d1170f358bfd
ATRAPA\myusoff <myusoff@deloitte.com>
2025-08-13 11:20:32 +0800

    Update analyze_code - make the children node can collapse upon clicking

---
+++ sample_project/your_code.java
@@ -0,0 +1,9 @@
+package sample_project;
+
+public class your_code {
+
+    public void main(String[] args) {
+        String name = "Ray";
+        System.out.println(name);
+    }
+}
--- your_code.java
+++
@@ -1,4 +0,0 @@
-public static void main(String[] args) {
-    String name = "Ray";
-    System.out.println(name);
-}
